b83d30742c5437e52a9c78aec558bd7fe9064193
jurjan123 <jansoniusjur@gmail.com>
2022-05-18 14:27:33 +0200

    first commit webshop

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>the whiskey shop</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">the whiskey shop</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="img/Jameson-Emblem.jpg" width="60px" height="60px" class="jameson"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="img/jack.jpg" width="60px" height="60px"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"><img src="img/R.jfif" width="60" height="60"></a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link"><img src="img/johnnie.png" width="60" height="60"></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
